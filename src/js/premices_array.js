d2fa90875783a8b013c4b3932fa6c54c39d76731
OlgaSld <125216168+OlgaSld@users.noreply.github.com>
2023-10-09 17:35:54 +0300

    add 308, del 013

--- src/js/premices_array.js
+++ src/js/premices_array.js
@@ -1,11 +1,11 @@
-import img013_1_more_mob from '../images/premises/013_mob/013_471_354_01-min.jpg';
-import img013_2_more_mob from '../images/premises/013_mob/013_471_354_02-min.jpg';
-import img013_3_more_mob from '../images/premises/013_mob/013_471_354_03-min.jpg';
-import img013_4_more_mob from '../images/premises/013_mob/013_471_354_04-min.jpg';
-import img013_1_more_desc from '../images/premises/013_desc/013_1440_1024_01-desc.jpg';
-import img013_2_more_desc from '../images/premises/013_desc/013_1440_1024_02-desc.jpg';
-import img013_3_more_desc from '../images/premises/013_desc/013_1440_1024_03-desc.jpg';
-import img013_4_more_desc from '../images/premises/013_desc/013_1440_1024_04-desc.jpg';
+// import img013_1_more_mob from '../images/premises/013_mob/013_471_354_01-min.jpg';
+// import img013_2_more_mob from '../images/premises/013_mob/013_471_354_02-min.jpg';
+// import img013_3_more_mob from '../images/premises/013_mob/013_471_354_03-min.jpg';
+// import img013_4_more_mob from '../images/premises/013_mob/013_471_354_04-min.jpg';
+// import img013_1_more_desc from '../images/premises/013_desc/013_1440_1024_01-desc.jpg';
+// import img013_2_more_desc from '../images/premises/013_desc/013_1440_1024_02-desc.jpg';
+// import img013_3_more_desc from '../images/premises/013_desc/013_1440_1024_03-desc.jpg';
+// import img013_4_more_desc from '../images/premises/013_desc/013_1440_1024_04-desc.jpg';
 import img014_1_more_mob from '../images/premises/014_mob/014_471_354_01-min.jpg';
 import img014_2_more_mob from '../images/premises/014_mob/014_471_354_02-min.jpg';
 import img014_3_more_mob from '../images/premises/014_mob/014_471_354_03-min.jpg';
@@ -38,71 +38,89 @@ import img313_1_more_desc from '../images/premises/313_desc/313_1440_1024_01-min
 import img313_2_more_desc from '../images/premises/313_desc/313_1440_1024_02-min.jpg';
 import img313_3_more_desc from '../images/premises/313_desc/313_1440_1024_03-min.jpg';
 import img313_4_more_desc from '../images/premises/313_desc/313_1440_1024_04-min.jpg';
-
+import img308_1_more_mob from '../images/premises/308_mob/308_471_354_01-min.jpg';
+import img308_2_more_mob from '../images/premises/308_mob/308_471_354_02-min.jpg';
+import img308_3_more_mob from '../images/premises/308_mob/308_471_354_04-min.jpg';
+import img308_4_more_mob from '../images/premises/308_mob/308_471_354_05-min.jpg';
+import img308_1_more_desc from '../images/premises/308_desc/308_1440_1024_1-min.jpg';
+import img308_2_more_desc from '../images/premises/308_desc/308_1440_1024_4-min.jpg';
+import img308_3_more_desc from '../images/premises/308_desc/308_1440_1024_5-min.jpg';
+import img308_4_more_desc from '../images/premises/308_desc/308_1440_1024_6-min.jpg';
 
 export const morePremices = [
-    {   
-        id: '13',
-        img1m: img013_1_more_mob,
-        img2m: img013_2_more_mob,
-        img3m: img013_3_more_mob,
-        img4m: img013_4_more_mob,
-        img1d: img013_1_more_desc,
-        img2d: img013_2_more_desc,
-        img3d: img013_3_more_desc,
-        img4d: img013_4_more_desc,
-        src: '013',
-    },
-    {
-        id: '14',
-        img1m: img014_1_more_mob,
-        img2m: img014_2_more_mob,
-        img3m: img014_3_more_mob,
-        img4m: img014_4_more_mob,
-        img1d: img014_1_more_desc,
-        img2d: img014_2_more_desc,
-        img3d: img014_3_more_desc,
-        img4d: img014_4_more_desc,
-        src: '014',
-    },
-    {
-        id: '15',
-        img1m: img015_1_more_mob,
-        img2m: img015_2_more_mob,
-        img3m: img015_3_more_mob,
-        img4m: img015_4_more_mob,
-        img1d: img015_1_more_desc,
-        img2d: img015_2_more_desc,
-        img3d: img015_3_more_desc,
-        img4d: img015_4_more_desc,
-        src: '015',
-    },
-    {
-        id: '16',
-        img1m: img016_1_more_mob,
-        img2m: img016_2_more_mob,
-        img3m: img016_3_more_mob,
-        img4m: img016_4_more_mob,
-        img1d: img016_1_more_desc,
-        img2d: img016_2_more_desc,
-        img3d: img016_3_more_desc,
-        img4d: img016_4_more_desc,
-        src: '016',
-    },
-    {
-        id: '313',
-        img1m: img313_1_more_mob,
-        img2m: img313_2_more_mob,
-        img3m: img313_3_more_mob,
-        img4m: img313_4_more_mob,
-        img1d: img313_1_more_desc,
-        img2d: img313_2_more_desc,
-        img3d: img313_3_more_desc,
-        img4d: img313_4_more_desc,
-        src: '313',
-    },
-
-]
+  // {
+  //     id: '13',
+  //     img1m: img013_1_more_mob,
+  //     img2m: img013_2_more_mob,
+  //     img3m: img013_3_more_mob,
+  //     img4m: img013_4_more_mob,
+  //     img1d: img013_1_more_desc,
+  //     img2d: img013_2_more_desc,
+  //     img3d: img013_3_more_desc,
+  //     img4d: img013_4_more_desc,
+  //     src: '013',
+  // },
+  {
+    id: '14',
+    img1m: img014_1_more_mob,
+    img2m: img014_2_more_mob,
+    img3m: img014_3_more_mob,
+    img4m: img014_4_more_mob,
+    img1d: img014_1_more_desc,
+    img2d: img014_2_more_desc,
+    img3d: img014_3_more_desc,
+    img4d: img014_4_more_desc,
+    src: '014',
+  },
+  {
+    id: '15',
+    img1m: img015_1_more_mob,
+    img2m: img015_2_more_mob,
+    img3m: img015_3_more_mob,
+    img4m: img015_4_more_mob,
+    img1d: img015_1_more_desc,
+    img2d: img015_2_more_desc,
+    img3d: img015_3_more_desc,
+    img4d: img015_4_more_desc,
+    src: '015',
+  },
+  {
+    id: '16',
+    img1m: img016_1_more_mob,
+    img2m: img016_2_more_mob,
+    img3m: img016_3_more_mob,
+    img4m: img016_4_more_mob,
+    img1d: img016_1_more_desc,
+    img2d: img016_2_more_desc,
+    img3d: img016_3_more_desc,
+    img4d: img016_4_more_desc,
+    src: '016',
+  },
+  {
+    id: '308',
+    img1m: img308_1_more_mob,
+    img2m: img308_2_more_mob,
+    img3m: img308_3_more_mob,
+    img4m: img308_4_more_mob,
+    img1d: img308_1_more_desc,
+    img2d: img308_2_more_desc,
+    img3d: img308_3_more_desc,
+    img4d: img308_4_more_desc,
+    src: '308',
+  },
+  {
+    id: '313',
+    img1m: img313_1_more_mob,
+    img2m: img313_2_more_mob,
+    img3m: img313_3_more_mob,
+    img4m: img313_4_more_mob,
+    img1d: img313_1_more_desc,
+    img2d: img313_2_more_desc,
+    img3d: img313_3_more_desc,
+    img4d: img313_4_more_desc,
+    src: '313',
+  },
+];
 
 // import img1_mob from '../images/premises/013_mob/013_273_322-min.jpg';
 // import img2_mob from '../images/premises/014_mob/014_273_322-min.jpg';
@@ -113,9 +131,8 @@ export const morePremices = [
 // import img3_desc from '../images/premises/015_desc/015_364_5_429_8-desc.jpg';
 // import img4_desc from '../images/premises/016_desc/016_364_5_429_8-desc.jpg'
 
-
 // export const freePremises = [
-//     {   
+//     {
 //         id: 1,
 //         img: img1_mob,
 //         img2: img1_desc,
