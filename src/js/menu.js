8ade405fbd233486d277297f3231110af684dc57
OlgaSld <125216168+OlgaSld@users.noreply.github.com>
2024-04-10 11:16:26 +0300

    fixed scroll

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,25 +1,25 @@
-  (() => {
-    const refs = {
-      openMenuBtn: document.querySelectorAll('[data-menu-open]'),
-      closeMenuBtn: document.querySelector('[data-menu-close]'),
-      menu: document.querySelector('[data-menu]'),
-      link: document.querySelector('.mob-menu-list')
-    };
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelectorAll('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    link: document.querySelector('.mob-menu-list'),
+  };
 
-    refs.link.addEventListener('click', onClose);
-  
-    refs.openMenuBtn.forEach((el) => {
-      el.addEventListener('click', toggleMenu);
-    });
-  
-    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.link.addEventListener('click', onClose);
 
-      function toggleMenu() {
-      refs.menu.classList.toggle('is-hidden');
-      document.body.classList.toggle('no-scroll');
-    }
+  refs.openMenuBtn.forEach(el => {
+    el.addEventListener('click', toggleMenu);
+  });
 
-    function onClose() {
-            refs.menu.classList.add('is-hidden');
-    }
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    // document.body.classList.toggle('no-scroll');
+  }
+
+  function onClose() {
+    refs.menu.classList.add('is-hidden');
+  }
 })();
--- src/js/scroll-top.js
+++ src/js/scroll-top.js
@@ -1,21 +1,21 @@
 const scrollTop = document.querySelector('.scroll');
 
-scrollTop.addEventListener('click', onScrollTopClick)
-window.addEventListener('scroll', trackScroll)
+scrollTop.addEventListener('click', onScrollTopClick);
+window.addEventListener('scroll', trackScroll);
 
 function trackScroll() {
-   const height = window.scrollY;
-   const coords = document.documentElement.clientHeight;
-    if (height > coords) {
-        scrollTop.classList.add('show')
-    } else {
-         scrollTop.classList.remove('show')
-    }
+  const height = window.scrollY;
+  const coords = document.documentElement.clientHeight;
+  if (height > coords) {
+    scrollTop.classList.add('show');
+  } else {
+    scrollTop.classList.remove('show');
+  }
 }
 
-export function onScrollTopClick () {
-    if (window.scrollY > 0) {
-        window.scrollBy(0, -30)
-        setTimeout(onScrollTopClick,0)
-    }
+export function onScrollTopClick() {
+  if (window.scrollY > 0) {
+    window.scrollBy(0, -30);
+    setTimeout(onScrollTopClick, 0);
+  }
 }
